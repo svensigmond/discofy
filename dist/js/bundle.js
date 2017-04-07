41a7e61d4e91064d32c19cad477b61d40f4130d9
Edwin Kalkman <edwin.kalkman@vicompany.nl>
2017-04-07 12:41:43 +0200

    mediaplayer setup

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,4 +1,16 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+class Spotify {
+	constructor() {
+		this.baseUrl = 'https://api.spotify.com/v1/';
+	}
+
+	searchTrack(query) {
+		const url = `${this.baseUrl}search?type=track&q=${query}`;
+
+		return fetch(url).then(response => response.json());
+	}
+}
+
 class Discogs {
 	constructor() {
 		this.token = 'tCCPbvQBMjhlVMqrIkjKWkpLduNeOXXQgwhWjWQs';
@@ -12,7 +24,7 @@ class Discogs {
 	}
 
 	getCollectionData(id, url) {
-		url = url || `${this.baseUrl}/users/${id}/collection/folders/0/releases?page=1&token=${this.token}`;
+		url = url || `${this.baseUrl}/users/${id}/collection/folders/0/releases?page=1&sort=artist&token=${this.token}`;
 
 		return fetch(url).then(response => response.json());
 	}
@@ -25,7 +37,15 @@ class Discogs {
 }
 
 const discogs = new Discogs();
+const spotify = new Spotify();
+
 const { Moon } = window;
+
+const MEDIAPLAYER_TEMPLATE = `<div class="mediaplayer">
+									<audio src="{{src}}">
+										Your browser does not support the <code>audio</code> element.
+									</audio>
+								</div>`;
 
 const ALBUM_TEMPLATE = `<div class="album" m-on:click="setAlbumData()">
 							<div class="album__visual">
@@ -86,7 +106,7 @@ const ALBUM_DETAILS_TEMPLATE = `<div class="album-details">
 												<th>Track list:</th>
 												<td>
 													<ol m-if="{{details.meta.trackList}}">
-														<li m-for="track in {{details.meta.trackList}}">{{track}}</li>
+														<li m-for="track in {{details.meta.trackList}}">{{track}} <button m-on:click="clickTrack({{track}})">🎵</button></li>
 													</ol>
 												</td>
 											</tr>
@@ -112,10 +132,15 @@ const discofy = new Moon({
 		details: {
 			show: false,
 		},
+		mediaUrl: null,
 	},
 
 	hooks: {
 		mounted() {
+			this.on('mediaplayer:play', (url) => {
+				this.set('mediaUrl', url);
+			});
+
 			this.on('update:details', (data) => {
 				this.set('details', data.album);
 				this.set('details.show', true);
@@ -131,16 +156,16 @@ const discofy = new Moon({
 
 		setUserData() {
 			discogs.getUserData(this.get('id'))
-			.then((response) => {
-				this.set('user', {
-					show: true,
-					name: response.name,
-					username: response.username,
-					avatar: response.avatar_url,
-					ownedAmount: response.num_collection,
-					location: response.location,
+				.then((response) => {
+					this.set('user', {
+						show: true,
+						name: response.name,
+						username: response.username,
+						avatar: response.avatar_url,
+						ownedAmount: response.num_collection,
+						location: response.location,
+					});
 				});
-			});
 		},
 
 		setCollectionData(url) {
@@ -186,25 +211,48 @@ Moon.component('component-album', {
 			const album = this.get('album');
 
 			discogs.getAlbumData(album.id)
-			.then((response) => {
-				album.meta = {
-					genres: response.genres.join(', '),
-					styles: response.styles ? response.styles.join(', ') : null,
-					art: response.images[0].uri,
-					trackList: response.tracklist.map((track) => track.title),
-					discogsUrl: response.uri,
-					formats: response.formats.map((format) => {
-						const descriptions = format.descriptions.join(', ');
+				.then((response) => {
+					album.meta = {
+						genres: response.genres.join(', '),
+						styles: response.styles ? response.styles.join(', ') : null,
+						art: response.images[0].uri,
+						trackList: response.tracklist.map((track) => track.title),
+						discogsUrl: response.uri,
+						formats: response.formats.map((format) => {
+							const descriptions = format.descriptions.join(', ');
 
-						return `${format.qty}x ${descriptions}`;
-					}),
-				};
+							return `${format.qty}x ${descriptions}`;
+						}),
+					};
 
-				this.set('album', album);
-			})
-			.then(() => {
-				discofy.emit('update:details', this.$data);
+					this.set('album', album);
+				})
+				.then(() => {
+					discofy.emit('update:details', this.$data);
+				});
+		},
+	},
+});
+
+// const audio = new Audio();
+
+Moon.component('component-mediaplayer', {
+	props: ['src'],
+	template: MEDIAPLAYER_TEMPLATE,
+	hooks: {
+		mounted() {
+			this.player = this.$el.querySelector('audio');
+
+			this.on('mediaplayer:play', (data) => {
+				this.callMethod('playAudio');
 			});
+		},
+	},
+	methods: {
+		playAudio() {
+			// this.player.setAttribute('src', url);
+
+			this.player.play();
 		},
 	},
 });
@@ -212,6 +260,27 @@ Moon.component('component-album', {
 Moon.component('component-album-details', {
 	props: ['details'],
 	template: ALBUM_DETAILS_TEMPLATE,
+	methods: {
+		clickTrack(track) {
+			const details = this.get('details');
+			const artists = details.artists.join();
+			const query = `${artists} ${track}`;
+
+			spotify.searchTrack(query)
+				.then((response) => {
+					const tracks = response.tracks.items;
+					const url = tracks.length && tracks[0].preview_url;
+
+					if (!url) {
+						alert('Yo no track found!1! :(');
+
+						return;
+					}
+
+					discofy.emit('mediaplayer:play', [url]);
+				});
+		},
+	},
 });
 
 window.discofy = discofy;
